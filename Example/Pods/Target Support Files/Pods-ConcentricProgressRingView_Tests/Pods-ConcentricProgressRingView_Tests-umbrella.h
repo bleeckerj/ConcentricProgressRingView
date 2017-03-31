c649444249da1fa7f5b048142bc41fad2a311e34
Julian Bleecker <julian@omata.com>
2017-03-30 20:21:23 -0700

    Clean up

--- Example/Pods/Target Support Files/Pods-ConcentricProgressRingView_Tests/Pods-ConcentricProgressRingView_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ConcentricProgressRingView_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ConcentricProgressRingView_TestsVersionString[];
-
